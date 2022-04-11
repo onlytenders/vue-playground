56267623a17bd2718d45e38021fac5a2376d2b8c
saddenly <37728443+onlytenders@users.noreply.github.com>
2022-04-11 20:14:58 +0600

    practice

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'hello world',
+      classTitle: 'titleBlue'
+    }
+  },
+  methods: {
+    reverseString() {
+      this.message = this.message.split('').reverse().join('')
+    },
+
+    changeColor() {
+      if (this.classTitle == 'titleBlue') {
+        this.classTitle = 'titleRed'
+      } else {
+        this.classTitle = 'titleBlue'
+      }
+    }
+  }
+}
+</script>
+
+<template>
+<h1 :class="classTitle">{{ message }}</h1>
+<button @click="reverseString">reverse</button>
+<button @click="changeColor">color</button>
+</template>
+
+<style>
+.titleBlue {
+  color: blue;
+}
+
+.titleRed {
+  color: red;
+}
+</style>
